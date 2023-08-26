7e92b698ee81624a7fe638df8eddbdcaccf754c9
Michael <michael.spengler@sap.com>
2023-08-26 13:59:38 +0200

    add sneaky tricks to support the freedom fans

--- svelte-based-ui/prepare-deployment-via-gh-pages.sh
+++ svelte-based-ui/prepare-deployment-via-gh-pages.sh
@@ -1,5 +1,6 @@
 # this is probably only necessary if you deploy via gh pages
 
 sed -i'' -e 's/href="\//href=".\//g' ./dist/index.html
+sed -i'' -e 's/src="\//src=".\//g' ./dist/index.html
 
 cp -r ./dist ./../docs
