1cee42548efd2ae1cdbd548730e676b907cab587
michael spengler <43786652+michael-spengler@users.noreply.github.com>
2023-08-30 15:33:22 +0200

    Update freedom.sol

--- freedom.sol
+++ freedom.sol
@@ -18,8 +18,13 @@ import "@openzeppelin/contracts/access/Ownable.sol"; // onlyOwner is used for th
 contract Freedom is ERC20, Ownable {
 
     uint256 public fixedSupply = 24000000; // There is a fixed supply of 24.000.000 Coins
+    uint256 public numberOfFreedomFans = 0; // this number will hopefully increase over time
+    uint256 public numberOfApprovedFreedomFans = 0; // this number will hopefully also increase over time
+    uint256 public currentThresholdForBecomingFullyApproved = 1; // this threshold increases proportionally to the number of approved FreedomFans
 
-    struct FreedomFan {
+    bool public projectStarted = false; // each project shall start with at least one - hopefully trustworthy - person  
+
+    struct FreedomFan { // this structure represents the core data which is stored for each Freedom Fan
         address walletAddress;
         string proofLink;
         uint256 appliedOn;
@@ -27,24 +32,21 @@ contract Freedom is ERC20, Ownable {
         uint256 amountOfReceivedApprovals;
     }
 
-    mapping(address => uint256) public ids; 
-    mapping(uint256 => FreedomFan) public freedomFans; 
-    mapping(address => address[]) public approversForFreedomFan; 
-    mapping(address => uint256) public ethLiquidityProviders;  
-    mapping(address => uint256) public maxRewardPerApprover;  
+    mapping(address => uint256) public ids; // this mapping is used to optimize the performance of some functions of this smart contract
+    mapping(uint256 => FreedomFan) public freedomFans; // in this mapping we store the core data per id of each Freedom Fan
+    mapping(address => address[]) public approversForFreedomFan; // in this mapping we store the approvers of each Freedom Fan
+    mapping(address => uint256) public ethLiquidityProviders;  // in this mapping we store how much ETH liquidity each Freedom Fan provided
+    mapping(address => uint256) public maxRewardPerFreedomFan;  // in this mapping we store how much reward each freedom fan can claim if the liquidity is sufficient 
+    mapping(uint256 => address) invitedMembersOfTrustedInitiationCommunity; // in this mapping we store who shall be invited and approved already during the start of this project
 
-    uint256 public numberOfFreedomFans = 1; 
-    uint256 public numberOfApprovedFreedomFans = 0;
-    uint256 public currentThresholdForBecomingFullyApproved = 1;
-    
-    bool public trustedInitiationCommunityInvited = false; // each project shall start with at least one - hopefully trustworthy - person  
-    mapping(uint256 => address) invitedMembersOfTrustedInitiationCommunity; 
-
-    event LOGMessage(string);
+    event LOGMessage(string); // whenever we want to log any text, we can emit this event
+    event LOGAddress(address); // whenever we want to log any text, we can emit this event
     
     constructor() ERC20("Freedom", "FREE") {
         // with the following all 24.000.000 coins are transferred to this contract itself.
         _mint(address(this), fixedSupply * (10 ** decimals())); 
+        // invitedMembersOfTrustedInitiationCommunity[1] = 0x9E972a43B3B8D68cD70930697E16429E47E88151; // e.g. the developer
+        invitedMembersOfTrustedInitiationCommunity[1] = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; // e.g. the developer
     }
 
     // The following modifiers are executed as checks before specific functions (see then further below) to protect the correct flow
@@ -58,38 +60,48 @@ contract Freedom is ERC20, Ownable {
         require(freedomFans[ids[freedomFan]].approvedOn == 0, "only not yet approved freedom fans can be approved.");
         _;
     }
-    modifier onlyApprovedHolders() {
-        require((freedomFans[ids[msg.sender]].approvedOn > 0 && balanceOf(msg.sender) > 0), "only approved holders can do that.");
-        _;
-    }
-    
 
     // Freedom Fans can join by sending a prooflink to proof they love freedom, fairness & peace
-    function joinFreedomFans(string memory proofLink) external {
-        require(ids[msg.sender] == 0, "it seems this wallet already applied once.");
-        numberOfFreedomFans = numberOfFreedomFans + 1;
-        ids[msg.sender] = numberOfFreedomFans;
-        freedomFans[numberOfFreedomFans].walletAddress = msg.sender;
+    function joinFreedomFans(string memory proofLink) external {        
+        updateNumberOfFreedomFansAndIDs(msg.sender);
+        updateCoreDataForFreedomFan(msg.sender, proofLink);
+    }
+
+    function updateNumberOfFreedomFansAndIDs(address newFreedomFan) public {
+        require(ids[newFreedomFan] == 0, "this wallet already has an id.");
+        emit LOGAddress(newFreedomFan);
+        numberOfFreedomFans = numberOfFreedomFans + 1; 
+        ids[newFreedomFan] = numberOfFreedomFans;  
+    }
+
+    function updateCoreDataForFreedomFan(address newFreedomFan, string memory proofLink) public {
+        freedomFans[numberOfFreedomFans].walletAddress = newFreedomFan; 
         freedomFans[numberOfFreedomFans].proofLink = proofLink;
         freedomFans[numberOfFreedomFans].appliedOn = block.timestamp;
-        freedomFans[numberOfFreedomFans].approvedOn = 0; // not yet approved
+        freedomFans[numberOfFreedomFans].approvedOn = 0; // not yet approved 
         freedomFans[numberOfFreedomFans].amountOfReceivedApprovals = 0; 
     }
 
     // As soon as a new Freedom Fan is fully approved, he and his approvers are eligible to receive a reward until all Coins are distributed
-    function approveFreedomFan(address freedomFan) public avoidDuplicateApprovals(freedomFan) onlyApprovedHolders onlyNotYetApprovedFreedomFansCanBeApproved(freedomFan) {
+    function approveFreedomFan(address freedomFan) public avoidDuplicateApprovals(freedomFan) onlyNotYetApprovedFreedomFansCanBeApproved(freedomFan) {
+        require(freedomFans[ids[msg.sender]].approvedOn > 0 || (projectStarted == false && owner() == msg.sender), "only approved freedom fans or the owner at the beginning can do that.");
+
         freedomFans[ids[freedomFan]].amountOfReceivedApprovals++; 
+        freedomFans[numberOfFreedomFans].amountOfReceivedApprovals = 1; 
+        approversForFreedomFan[msg.sender].push(address(this)); 
+        numberOfApprovedFreedomFans = numberOfApprovedFreedomFans + 1;
+
         if (freedomFans[ids[freedomFan]].amountOfReceivedApprovals >= currentThresholdForBecomingFullyApproved) {
             freedomFans[ids[freedomFan]].approvedOn = block.timestamp;
-            if (balanceOf(address(this)) >= (1 * (10 ** 18))) {
-                maxRewardPerApprover[freedomFan] = 24 * (10 ** decimals()); // welcome gift to new freedom fan
+            if (balanceOf(address(this)) >= (24 * (10 ** 18))) {
+                maxRewardPerFreedomFan[freedomFan] = 24 * (10 ** decimals()); // welcome gift to new freedom fan
             } else {
                 emit LOGMessage("the fixed supply of overall 24.000.000 Coins / the balanceOf FREE of this contract does not allow welcome gifts atm. new approved freedom fans might invest in FREE via the buy function.");
             }
 
             for (uint i = 0; i < approversForFreedomFan[freedomFan].length; i++) {
                 if (balanceOf(address(this)) >= (1 * (10 ** 18))){
-                        maxRewardPerApprover[approversForFreedomFan[freedomFan][i]] = maxRewardPerApprover[approversForFreedomFan[freedomFan][i]] + 1 * (10 ** decimals());
+                        maxRewardPerFreedomFan[approversForFreedomFan[freedomFan][i]] = maxRewardPerFreedomFan[approversForFreedomFan[freedomFan][i]] + 1 * (10 ** decimals());
                 } else {
                     emit LOGMessage("the fixed supply of overall 24.000.000 Coins / the balanceOf FREE does not allow direct rewards for approvers atm. the incentives should anyways be high enough to continue approving wisely.");
                 }
@@ -102,32 +114,27 @@ contract Freedom is ERC20, Ownable {
 
     // The reason why I do not automatically send the rewards is that I think it is fair that the receiver of the rewards pays the gas fees
     function claimCurrentlyAvailableLiquidityBackedMaxRewards() public {
-        if (balanceOf(address(this)) >= maxRewardPerApprover[msg.sender]) {
-            this.transfer(msg.sender, maxRewardPerApprover[msg.sender]);
+        uint256 amountToBeTransferred;
+        if (balanceOf(address(this)) >= maxRewardPerFreedomFan[msg.sender]) {
+            amountToBeTransferred = maxRewardPerFreedomFan[msg.sender];
         } else {
-            this.transfer(msg.sender, balanceOf(address(this)));
+            amountToBeTransferred = balanceOf(address(this));
         }
+        this.transfer(msg.sender, amountToBeTransferred);
+        maxRewardPerFreedomFan[msg.sender] = maxRewardPerFreedomFan[msg.sender] - amountToBeTransferred;
     }
 
-
     function startProject() public { 
-        require(trustedInitiationCommunityInvited == false, "startProject can only be executed once.");
-        require(msg.sender == owner(), "startProject can only be executed by the owner. at the end of startProject this contract is automatically renounced.");
-
-        invitedMembersOfTrustedInitiationCommunity[1] = 0x9E972a43B3B8D68cD70930697E16429E47E88151; // lonely programmer atm :)
-
-        for (uint16 i; i < 1; i++){
-            ids[msg.sender] = numberOfFreedomFans;
-            freedomFans[numberOfFreedomFans].walletAddress = msg.sender;
-            freedomFans[numberOfFreedomFans].proofLink = "https://twitter.com/Peer2peerE/status/1695323724646322412"; // here one could imagine a general proof link related to all trusted freedom fans at the beginning of this project
-            freedomFans[numberOfFreedomFans].appliedOn = block.timestamp;
-            freedomFans[numberOfFreedomFans].approvedOn = block.timestamp; // not yet approved
-            freedomFans[numberOfFreedomFans].amountOfReceivedApprovals = 1; 
-            approversForFreedomFan[msg.sender].push(address(this)); 
-            numberOfApprovedFreedomFans = numberOfApprovedFreedomFans + 1;
-            this.transfer(msg.sender, 24 * (10 ** decimals()));
+        require(projectStarted == false && msg.sender == owner(), "startProject can only be executed once by the owner. at the end of startProject this contract is automatically renounced.");
+        for (uint16 i = 1; i <= 24; i++){ // assuming there are a maximum of 24 invited members of the trusted initiation community 
+            if (invitedMembersOfTrustedInitiationCommunity[i] == 0x0000000000000000000000000000000000000000) {
+                return;
+            }
+            updateNumberOfFreedomFansAndIDs(invitedMembersOfTrustedInitiationCommunity[i]);
+            updateCoreDataForFreedomFan(invitedMembersOfTrustedInitiationCommunity[i], "https://twitter.com/Peer2peerE/status/1695323724646322412");
+            approveFreedomFan(invitedMembersOfTrustedInitiationCommunity[i]);
         }
-        trustedInitiationCommunityInvited = true;
+        projectStarted = true;
         renounceOwnership();
     }
 
@@ -136,7 +143,8 @@ contract Freedom is ERC20, Ownable {
     // 1. be free from rather limiting dependencies and fees from liquidity pools, cexes and dexes      
     // 2. restrict max buying amount per holder to 1 Finney to support long term decentralization & fairness    
     // 3. ensure the price is defined by the network size and by the value of the network   
-    function buy() public payable onlyApprovedHolders { 
+    function buy() public payable  { 
+        require(freedomFans[ids[msg.sender]].approvedOn > 0, "only approved freedom fans can do that.");
         require(msg.value <= 1 * 10 ** 15, "you can buy for a maximum of one finney which is 0.001 ETH. chancellor on brink of second bailout for banks :)"); // this is meant to foster decentralization 
         require(msg.value + ethLiquidityProviders[msg.sender] <=  1 * 10 ** 15, "you can overall buy for a maximum of one finney which is 0.001 ETH. it seems you invested already earlier.");
         uint256 priceOfOneFREEInWEI = numberOfApprovedFreedomFans * (10**18) / 1000000000; // the more approved freedom fans there are, the higher the value of FREE
@@ -145,7 +153,8 @@ contract Freedom is ERC20, Ownable {
         this.transfer(msg.sender, amountToBeTransferred); 
     }
 
-    function sell(uint256 amountInWEI) external  onlyApprovedHolders {
+    function sell(uint256 amountInWEI) external {
+        require(freedomFans[ids[msg.sender]].approvedOn > 0, "only approved freedom fans can do that.");
         require(balanceOf(msg.sender) >= amountInWEI, "this smart contract does not support selling more than you have.");    
         require(allowance(msg.sender, address(this)) >= amountInWEI, "please set an appropriate allowance first.");    
         IERC20(address(this)).transferFrom(msg.sender, address(this), amountInWEI); 
